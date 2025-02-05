df76676a2808f8de73e95d76bdeaa42be24c13f4
Pablo Barbero <pablobarbero220@gmail.com>
2025-02-05 00:21:06 -0300

    fix: vercel

--- src/app.ts
+++ src/app.ts
@@ -25,4 +25,7 @@ app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
 
-export default app;
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
